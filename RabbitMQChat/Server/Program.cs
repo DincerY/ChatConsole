b3a850974d9c9b8b0d1f53e28bb02eaaacd9ce98
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-02-09 19:34:47 +0300

    RabbitMq Message Done

---
+++ RabbitMQChat/Server/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+
+ConnectionFactory factory = new ConnectionFactory();
+IConnection connection = factory.CreateConnection();
+
+
+IModel channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete:false,
+    arguments:null);
+
+
+Console.WriteLine("Çıkmak için hiç bir string değer yazmadan enter tuşuna basın");
+
+while (true)
+{
+    Console.Write("Mesaj Yazın : ");
+    string message = Console.ReadLine();
+    if (string.IsNullOrEmpty(message))
+    {
+        break;
+    }
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange: string.Empty,
+        routingKey: "hello",
+        basicProperties: null,
+        body: body);
+}
+
