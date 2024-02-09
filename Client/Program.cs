6aa9db6188404d64f692467a85c8483d4f6ec3a5
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-02-09 17:03:16 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,18 @@
+﻿HttpClient client = new();
+
+Uri requestUri = new Uri("http://localhost:8080");
+client.BaseAddress = requestUri;
+
+Console.WriteLine("Http Client Çalışıyor...");
+
+while (true)
+{
+    Console.WriteLine("Mesajı giriniz");
+    string contet = Console.ReadLine();
+    var content = new StringContent(contet);
+    var result = await client.PostAsync("/", content);
+    string resultContent = await result.Content.ReadAsStringAsync();
+    Console.WriteLine(resultContent);
+}
+
+
---
+++ Server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+
+string serverAddress = "http://localhost:8080/";
+
+HttpListener listener = new HttpListener();
+listener.Prefixes.Add(serverAddress);
+
+Console.WriteLine($"HTTP sunucu çalışıyor...");
+
+listener.Start();
+
+while (true)
+{
+    HttpListenerContext context = listener.GetContext();
+    HttpListenerRequest request = context.Request;
+    HttpListenerResponse response = context.Response;
+    string text = "";
+
+    if (request.HasEntityBody)
+    {
+        System.IO.Stream body = request.InputStream;
+        StreamReader reader = new StreamReader(body);
+        text = reader.ReadToEnd();
+        Console.WriteLine($"Request body : {text}");
+    }
+
+    string responseString = text == "" ? "Mesaj gelmedi" : $"{DateTime.Now} -- Bu mesajı attınız : {text}";
+    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+    response.ContentLength64 = buffer.Length;
+    System.IO.Stream output = response.OutputStream;
+    output.Write(buffer, 0, buffer.Length);
+    output.Close();
+}
+
+Console.WriteLine("Hello, World!");
+
+
